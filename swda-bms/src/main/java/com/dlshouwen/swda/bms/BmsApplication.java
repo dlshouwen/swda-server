9729bd5e428a52ff1a27f030207c810d31f5ed56
刘精诚 <liujingcheng@live.cn>
2024-06-27 10:45:02 +0800

    upload bms application

--- swda-bms/src/main/java/com/dlshouwen/swda/bms/BmsApplication.java
+++ swda-bms/src/main/java/com/dlshouwen/swda/bms/BmsApplication.java
@@ -5,12 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * bms application
+ * @author liujingcheng@live.cn
+ * @since 1.0.0
+ */
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class BmsApplication {
 
+	/**
+	 * main
+	 * @param args
+	 */
 	public static void main(String[] args) {
+//		spring application run
 		SpringApplication.run(BmsApplication.class, args);
 	}
 
